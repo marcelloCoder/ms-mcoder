eb650091c8056ac99c0de4b9fd462cf075623420
marcelloCoder <deoliveiral767@gmail.com>
2025-02-10 20:50:12 -0300

    hystrix add

--- hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
@@ -26,17 +26,23 @@ public class WorkerResource {
 
 	@Autowired
 	private WorkerRepository repository;
-	
+
 	@GetMapping
-	public ResponseEntity<List<Worker>> findAll(){
+	public ResponseEntity<List<Worker>> findAll() {
 		List<Worker> list = repository.findAll();
 		return ResponseEntity.ok(list);
 	}
-	
-	@GetMapping(value = "/{id}")
-	public ResponseEntity<Worker> findById(@PathVariable Long id){
 
-	logger.info("PORT = " + environment.getProperty("local.server.port"));
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Worker> findById(@PathVariable Long id) {
+
+		/*try {
+			Thread.sleep(3000L);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}*/
+
+		logger.info("PORT = " + environment.getProperty("local.server.port"));
 
 		Worker obj = repository.findById(id).get();
 		return ResponseEntity.ok(obj);
