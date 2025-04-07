4fb16a99503163cfbf84090485fbfaa1e46b327e
marcelloCoder <deoliveiral767@gmail.com>
2025-04-07 12:04:43 -0300

    zuul timeout

--- hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
@@ -36,11 +36,11 @@ public class WorkerResource {
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable Long id) {
 
-		/*try {
+		try {
 			Thread.sleep(3000L);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
-		}*/
+		}
 
 		logger.info("PORT = " + environment.getProperty("local.server.port"));
 
