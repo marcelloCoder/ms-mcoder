0dd5bab0c97995c285ddf9f4331902671b81a92c
marcelloCoder <deoliveiral767@gmail.com>
2025-12-18 15:13:56 -0300

    adding hr-config

---
+++ hr-config/src/main/java/com/devsuperior/hrconfig/HrconfigApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.hrconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrconfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrconfigApplication.class, args);
+	}
+
+}
---
+++ hr-config/src/test/java/com/devsuperior/hrconfig/HrconfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.hrconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrconfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
